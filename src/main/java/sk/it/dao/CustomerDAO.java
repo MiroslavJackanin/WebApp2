38b72996a0d6f994b486d8e0b7f4cd066ad0b891
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2020-12-14 12:07:10 +0100

    init

---
+++ src/main/java/sk/it/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package sk.it.dao;
+
+import sk.it.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    List<Customer> getCustomers();
+
+    void saveCustomer(Customer customer);
+
+    Customer getCustomer(int id);
+
+    void deleteCustomer(int id);
+}
---
+++ src/main/java/sk/it/dao/CustomerDAOImpl.java
@@ -0,0 +1,53 @@
+package sk.it.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import sk.it.entity.Customer;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Customer> getCustomers() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<Customer> query = session.createQuery("from Customer order by lastName");
+
+        return query.getResultList();
+    }
+
+    @Override
+    public void saveCustomer(Customer customer) {
+
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.saveOrUpdate(customer);
+
+    }
+
+    @Override
+    public Customer getCustomer(int id) {
+
+        Session currentSession = sessionFactory.getCurrentSession();
+        return currentSession.get(Customer.class, id);
+
+    }
+
+    @Override
+    public void deleteCustomer(int id) {
+
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        Query query = currentSession.createQuery("delete from Customer where id=:customerId");
+        query.setParameter("customerId", id);
+        query.executeUpdate();
+    }
+}
